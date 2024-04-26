d0dc6d2b8cb0c8cae05ca701fea876dc2cf9364f
Darren Ni <darren.ni@emory.edu>
2024-04-25 23:05:43 -0400

    debug

--- api/redshift_server.cjs
+++ api/redshift_server.cjs
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("/"));
 app.use(cors());
 app.use(express.json());
 // app.use(express.static())
