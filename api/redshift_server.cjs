59829ab2ac84cdaa2c0969060931e6a912585b21
Darren Ni <darren.ni@emory.edu>
2024-04-30 22:18:40 -0400

    clean comments

--- api/redshift_server.cjs
+++ api/redshift_server.cjs
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-const pool = new Pool({ //NEED TO SWITCH TO ENVIRONMENTAL VARIABLES
+const pool = new Pool({
   user: process.env.REDSHIFT_USER,
   host: process.env.REDSHIFT_HOST,
   database: process.env.REDSHIFT_DATABASE,
@@ -14,26 +14,24 @@ const pool = new Pool({ //NEED TO SWITCH TO ENVIRONMENTAL VARIABLES
 });
 
 app.get('/api/company_page', async (req, res) => {
-    try {
-      const queryResult = await pool.query(`SELECT name, industries, round, amount, round_valuation_usd, growth_stage, launch_year, total_rounds_number, current_company_valuation FROM coadata.master_table_stg`);
-      res.json(queryResult.rows); 
-    } catch (err) {
-      console.error('Error executing query:', err.stack);
-      res.status(500).send('Error fetching data'); 
-    }
+    try {
+      const queryResult = await pool.query(`SELECT name, industries, round, amount, round_valuation_usd, growth_stage, launch_year, total_rounds_number, current_company_valuation FROM coadata.master_table_stg`);
+      res.json(queryResult.rows); 
+    } catch (err) {
+      console.error('Error executing query:', err.stack);
+    res.status(500).send('Error fetching data'); 
+    }
   });
 
 app.get('/api/map', async (req, res) => {
-    try {
-      const queryResult = await pool.query(`SELECT latitude, longitude FROM coadata.locations`);
-      res.json(queryResult.rows); 
-    } catch (err) {
-      console.error('Error executing query:', err.stack);
-      res.status(500).send('Error fetching data'); 
-    }
+    try {
+      const queryResult = await pool.query(`SELECT latitude, longitude FROM coadata.locations`);
+      res.json(queryResult.rows); 
+    } catch (err) {
+      console.error('Error executing query:', err.stack);
+      res.status(500).send('Error fetching data'); 
+    }
   });
-
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -136,7 +136,6 @@ const ecosystemValueByCity = [
   { city: "Charlotte", "Ecosystem Value": 11 },
 ];
 
-// in progress
 
 const totalFundingByYear = [
   { date: "Jan 23", "Total Funding": 500, "Pre-Seed Funding": 100 },
@@ -223,15 +222,9 @@ const ScatterChartUsageExampleWithClickEvent = () => {
   const axisOptions = {
     "Ecosystem Value": "current_company_valuation",
     "Funding Rounds": "total_rounds_number",
-    "Total Funding": "amount", // need to add this to master table
-    // "Number of Startups per Year": "numberOfStartups",
-    // "Minority-founded Startups": "minorityStartups",
-    // "Type of Startup": "startupType",
+    "Total Funding": "amount",
     "Funding Year" : "launch_year",
     "Round Evaluation": "round_valuation_usd",
-    // GDP: "GDP",
-    // "Life expectancy": "Life expectancy",
-    // Population: "Population",
   };
 
   return (
@@ -267,8 +260,7 @@ const ScatterChartUsageExampleWithClickEvent = () => {
       <ScatterChart
         className="-ml-2 mt-6 h-80"
         yAxisWidth={50}
-        // minXValue={0}
-        // maxXValue={100000000}
+
         data={chartData}
         category="name"
         x={label[xAxis]}
@@ -278,11 +270,6 @@ const ScatterChartUsageExampleWithClickEvent = () => {
         showOpacity={true}
         minYValue={0}
         showLegend={false}
-        // valueFormatter={{
-        //   x: (amount) => `$${(amount / 1000).toFixed(1)}K`,
-        // y: (amount) => `${amount/100000} $`,
-        //   size: (amount) => `${(amount / 1000000).toFixed(1)}M people`,
-        // }}
       />
     </Card>
   );
